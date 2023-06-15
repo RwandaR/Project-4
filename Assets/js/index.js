762237ef4a2468f37d3ce1f08768ee488317fcfe
emellyb1 <emellybeltran@gmail.com>
2023-06-14 22:27:02 -0400

    completed task 4 - Task Form Inputs Validation

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,12 +1,32 @@
-const validFormFieldInput = document.querySelector('#taskName');
 
-const button =document.querySelector('#taskButton');
-function validate() {
-    const createTaskName = validFormFieldInput.value;
-    if (typeof createTaskName === 'string') {
-        console.log(createTaskName);
-    } else {
+//Task Name Form Input
+const newTaskNameInput = document.querySelector('#taskName');
+const newTaskDescriptionInput = document.querySelector('#taskDescription');
+const newAssignedTo = document.querySelector('#assignedTo');
+const newDueDate = document.querySelector('#dueDate');
+
+const taskButton = document.querySelector('#taskButton');
+function validate(event) {
+    event.preventDefault();
+    const createTaskName = newTaskNameInput.value;
+    console.log(createTaskName);
+    if (createTaskName == '') {
         alert('Please insert a valid task name');
     }
+    const createDescription = newTaskDescriptionInput.value;
+    console.log(createDescription);
+    if (createDescription == '') {
+        alert('Please insert a valid task description');
+    }
+    const createAssignedTo = newAssignedTo.value;
+    console.log(createAssignedTo);
+    if (createAssignedTo == false) {
+        alert('Please select a name to assign this task');
+    }
+    const createDueDate = newDueDate.value;
+    console.log(createDueDate);
+    if (createDueDate == false) {
+        alert('Please select a due date for this task');
+    }
 }
-button.addEventListener('click', validate);
+taskButton.addEventListener('click', validate);
